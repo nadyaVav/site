ef71745b0a0fce991159115f18802dd6d729669f
Nadya Vavilova <nadya.dem.vav@gmail.com>
2016-09-01 00:33:23 +0300

    Для динамического сайта

---
+++ blocks/header.php
@@ -0,0 +1,14 @@
+<?php
+/**
+* Created by PhpStorm.
+* User: qwerty
+* Date: 01.09.2016
+* Time: 0:26
+*/
+?>
+<table width="650px" border="1" cellspacing="0" cellpadding="0" align="center" bordercolor="313233" bgcolor="#cbe3ec">
+    <tr>
+        <td><p align="center">Шапка сайта</p></br></td>
+    </tr>
+</table>
+
---
+++ blocks/leftmenu.php
@@ -0,0 +1,17 @@
+<?php
+/**
+* Created by PhpStorm.
+* User: qwerty
+* Date: 01.09.2016
+* Time: 0:28
+*/
+?>
+<td colspan="2" style="font-size: 14px; font-family: Arial" bgcolor="#ecf8fc">
+    <ul>
+        <li><a href="#">Ссылка 1</a></li>
+        <li><a href="#">Ссылка 2</a></li>
+        <li><a href="#">Ссылка 3</a></li>
+        <li><a href="#">Ссылка 4</a></li>
+        <li><a href="#">Ссылка 5</a></li>
+    </ul>
+</td>
