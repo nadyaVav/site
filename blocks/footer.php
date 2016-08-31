b84511e0fdaed467e1eafc49b75ab9cb4cdc4abb
Nadya Vavilova <nadya.dem.vav@gmail.com>
2016-09-01 00:38:26 +0300

    Для динамического сайта

--- blocks/footer.php
+++ blocks/footer.php
@@ -1,11 +1,3 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: qwerty
- * Date: 01.09.2016
- * Time: 0:29
- */
-?>
 <table width="650px" border="1" cellspacing="0" cellpadding="0" align="center" bordercolor="313233" bgcolor="#cbe3ec">
     <tr>
         <td><p align="center">Подвал сайта</p></br></td>
--- blocks/leftmenu.php
+++ blocks/leftmenu.php
@@ -1,11 +1,4 @@
-<?php
-/**
-* Created by PhpStorm.
-* User: qwerty
-* Date: 01.09.2016
-* Time: 0:28
-*/
-?>
+
 <td colspan="2" style="font-size: 14px; font-family: Arial" bgcolor="#ecf8fc">
     <ul>
         <li><a href="#">Ссылка 1</a></li>
